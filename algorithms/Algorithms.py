082c89a757754be1e0fdec709e4f6e006852d2e1
Mateusz Latkowski <mateusz.latkowski@m6.smcebi.us.edu.pl>
2018-12-11 21:09:31 +0100

    Add FibonacciNumber class

--- algorithms/Algorithms.py
+++ algorithms/Algorithms.py
@@ -78,7 +78,7 @@ class FermatFactorization:
 class BubbleSort:
     
     def __init__(self):
-        self.nazwa = "Algorytm Sortowania Bubble Sort"
+        self.name = "Algorytm Sortowania Bubble Sort"
         
     def bsort(self, data):
         """Is a simple algorithm that repeatedly steps through the list, compares adjacent pairs and swaps them if they are in the wrong order"""
@@ -89,9 +89,33 @@ class BubbleSort:
         return data
     
     def __repr__(self):
-        return self.nazwa
+        return self.name
     
     def __str__(self):
         return self.__repr__()
+    
+    
+class FibonacciNumber:
+    
+    def __init__(self):
+        self.name = "Ciag Fibonacciego"
+        
+    def fibonacci(self, N):
+        """Fibonacci sequence, and characterized by the fact that every number after the first two is the sum of the two preceding ones"""
+        data = [0, 1]
+        for i in range(2, N+1):
+            score = data[i - 1] + data[i - 2]
+            data.append(score)
+            
+        return data
+    
+    def __repr__(self):
+        return self.name
+    
+    def __str__(self):
+        return self.__repr__()
+    
+    
+    
     
 
