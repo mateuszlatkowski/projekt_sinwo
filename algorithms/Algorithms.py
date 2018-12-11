76d477784b41954e97edfce92e75cd29c221bf06
Mateusz Latkowski <mateusz.latkowski@m6.smcebi.us.edu.pl>
2018-12-11 20:40:36 +0100

    Add FermatFactorization class to Algorithms.py file

--- algorithms/Algorithms.py
+++ algorithms/Algorithms.py
@@ -29,7 +29,7 @@ class EuclideanAlgorithm:
 class LeastCommonMultiple(EuclideanAlgorithm):
     
     def __init__(self):
-        self.nazwa = "Najwieksza Wspolna Wielokrotnosc"
+        self.name = "Najwieksza Wspolna Wielokrotnosc"
         
     def NWW(self, a, b):
         """Is the smallest positive integer that is divisible by both a and b"""
@@ -37,19 +37,39 @@ class LeastCommonMultiple(EuclideanAlgorithm):
         return nww
     
     def __repr__(self):
-        return self.nazwa
+        return self.name
     
     def __str__(self):
         return self.__repr__()
 
     
-class klasa3:
+class FermatFactorization:
     
     def __init__(self):
-        self.nazwa_funkcji = "funkcja3"
+        self.name = "Algorytm Fermata"
+        
+    def fermat(self, N):
+        """The factorization method, the distriubtion of the number into prime factors"""
+        if N <= 0:
+            return 0
+    
+        i  = 2
+        e = math.floor(math.sqrt(N))
+        numbers = []
+    
+        while i <= e:
+            if N % i == 0:
+                numbers.append(i)
+                N /= i
+                e = e = math.floor(math.sqrt(N))
+            else:
+                i += 1
+    
+        if N > 1: numbers.append(N)
+        return numbers
     
     def __repr__(self):
-        return "test klasy3"
+        return self.name
     
     def __str__(self):
         return self.__repr__()
